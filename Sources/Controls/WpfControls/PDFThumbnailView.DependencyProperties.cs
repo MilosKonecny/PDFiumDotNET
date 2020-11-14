9d86688e0d10065169627a0e4ca17ce47ed54137
Miloš Konečný <milos.konecny@outlook.com>
2020-11-14 20:47:59 +0100

    Add PDFThumbnailView

---
+++ Sources/Controls/WpfControls/PDFThumbnailView.DependencyProperties.cs
@@ -0,0 +1,199 @@
+﻿namespace PDFiumDotNET.WpfControls
+{
+    using System.ComponentModel;
+    using System.Windows;
+    using System.Windows.Media;
+    using PDFiumDotNET.Components.Contracts.Page;
+
+    /// <summary>
+    /// View class shows page thumbnails from opened PDF document.
+    /// </summary>
+    public partial class PDFThumbnailView
+    {
+        #region Dependency properties - register
+
+        /// <summary>
+        /// Dependency property for 'PDFPageBackground' - source of information to draw content.
+        /// </summary>
+        public static readonly DependencyProperty PDFPageBackgroundProperty
+            = DependencyProperty.Register("PDFPageBackground", typeof(Brush), typeof(PDFThumbnailView),
+                new FrameworkPropertyMetadata(Brushes.White, HandlePDFPageBackgroundPropertyChanged));
+
+        /// <summary>
+        /// Dependency property for 'PDFPageBorderBrush' - source of information to draw content.
+        /// </summary>
+        public static readonly DependencyProperty PDFPageBorderBrushProperty
+            = DependencyProperty.Register("PDFPageBorderBrush", typeof(Brush), typeof(PDFThumbnailView),
+                new FrameworkPropertyMetadata(Brushes.Black, HandlePDFPageBorderBrushPropertyChanged));
+
+        /// <summary>
+        /// Dependency property for 'PDFPageBorderThickness' - source of information to draw content.
+        /// </summary>
+        public static readonly DependencyProperty PDFPageBorderThicknessProperty
+            = DependencyProperty.Register("PDFPageBorderThickness", typeof(Thickness), typeof(PDFThumbnailView),
+                new FrameworkPropertyMetadata(new Thickness(0.5), HandlePDFPageBorderThicknessPropertyChanged));
+
+        /// <summary>
+        /// Dependency property for 'PDFPageComponent' - source of information to draw content.
+        /// </summary>
+        public static readonly DependencyProperty PDFPageComponentProperty
+            = DependencyProperty.Register("PDFPageComponent", typeof(IPDFPageComponent), typeof(PDFThumbnailView),
+                new FrameworkPropertyMetadata(null, HandlePDFPageComponentPropertyChanged));
+
+        #endregion Dependency properties - register
+
+        #region Dependency properties - properties
+
+        /// <summary>
+        /// Gets or sets the value of dependency property.
+        /// </summary>
+        public Brush PDFPageBackground
+        {
+            get => (Brush)GetValue(PDFPageBackgroundProperty);
+            set => SetValue(PDFPageBackgroundProperty, value);
+        }
+
+        /// <summary>
+        /// Gets or sets the value of dependency property.
+        /// </summary>
+        public Brush PDFPageBorderBrush
+        {
+            get => (Brush)GetValue(PDFPageBorderBrushProperty);
+            set => SetValue(PDFPageBorderBrushProperty, value);
+        }
+
+        /// <summary>
+        /// Gets or sets the value of dependency property.
+        /// </summary>
+        public Thickness PDFPageBorderThickness
+        {
+            get => (Thickness)GetValue(PDFPageBorderThicknessProperty);
+            set => SetValue(PDFPageBorderThicknessProperty, value);
+        }
+
+        /// <summary>
+        /// Gets or sets the value of dependency property.
+        /// </summary>
+        public IPDFPageComponent PDFPageComponent
+        {
+            get => (IPDFPageComponent)GetValue(PDFPageComponentProperty);
+            set => SetValue(PDFPageComponentProperty, value);
+        }
+
+        #endregion Dependency properties - properties
+
+        #region Dependency properties - private callback methods
+
+        /// <summary>
+        /// Callback method is called whenever <see cref="PDFPageBackground"/> property has changed value.
+        /// </summary>
+        /// <param name="o">The <see cref="DependencyObject"/> on which the property has changed value.</param>
+        /// <param name="e">Event data that is issued by any event that tracks changes to the effective value of this property.</param>
+        private static void HandlePDFPageBackgroundPropertyChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
+        {
+            var view = o as PDFThumbnailView;
+            if (view == null)
+            {
+                return;
+            }
+
+            view.InvalidateVisual();
+        }
+
+        /// <summary>
+        /// Callback method is called whenever <see cref="PDFPageBorderBrush"/> property has changed value.
+        /// </summary>
+        /// <param name="o">The <see cref="DependencyObject"/> on which the property has changed value.</param>
+        /// <param name="e">Event data that is issued by any event that tracks changes to the effective value of this property.</param>
+        private static void HandlePDFPageBorderBrushPropertyChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
+        {
+            var view = o as PDFThumbnailView;
+            if (view == null)
+            {
+                return;
+            }
+
+            view.InvalidateVisual();
+        }
+
+        /// <summary>
+        /// Callback method is called whenever <see cref="PDFPageBorderThickness"/> property has changed value.
+        /// </summary>
+        /// <param name="o">The <see cref="DependencyObject"/> on which the property has changed value.</param>
+        /// <param name="e">Event data that is issued by any event that tracks changes to the effective value of this property.</param>
+        private static void HandlePDFPageBorderThicknessPropertyChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
+        {
+            var view = o as PDFThumbnailView;
+            if (view == null)
+            {
+                return;
+            }
+
+            view.InvalidateVisual();
+        }
+
+        /// <summary>
+        /// Callback method is called whenever <see cref="PDFPageComponent"/> property has changed value.
+        /// </summary>
+        /// <param name="o">The <see cref="DependencyObject"/> on which the property has changed value.</param>
+        /// <param name="e">Event data that is issued by any event that tracks changes to the effective value of this property.</param>
+        private static void HandlePDFPageComponentPropertyChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
+        {
+            var view = o as PDFThumbnailView;
+            if (view == null)
+            {
+                return;
+            }
+
+            if (e.OldValue != null)
+            {
+                view.Unuse(e.OldValue as IPDFPageComponent);
+            }
+
+            if (e.NewValue != null)
+            {
+                view.Use(e.NewValue as IPDFPageComponent);
+            }
+        }
+
+        #endregion Dependency properties - private callback methods
+
+        #region Private methods
+
+        private void Use(IPDFPageComponent component)
+        {
+            if (component == null)
+            {
+                return;
+            }
+
+            component.PropertyChanged += HandlePDFPageComponentPropertyChangedEvent;
+            ScrollOwner?.InvalidateScrollInfo();
+        }
+
+        private void Unuse(IPDFPageComponent component)
+        {
+            if (component == null)
+            {
+                return;
+            }
+
+            component.PropertyChanged -= HandlePDFPageComponentPropertyChangedEvent;
+            ScrollOwner?.InvalidateScrollInfo();
+        }
+
+        #endregion Private methods
+
+        #region Private event handler methods
+
+        private void HandlePDFPageComponentPropertyChangedEvent(object sender, PropertyChangedEventArgs e)
+        {
+            if (!string.Equals(nameof(IPDFPageComponent.CurrentPageIndex), e.PropertyName))
+            {
+                InvalidateVisual();
+            }
+        }
+
+        #endregion Private event handler methods
+    }
+}
---
+++ Sources/Controls/WpfControls/PDFThumbnailView.Render.cs
@@ -0,0 +1,138 @@
+﻿namespace PDFiumDotNET.WpfControls
+{
+    using System;
+    using System.Globalization;
+    using System.Linq;
+    using System.Windows;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using PDFiumDotNET.WpfControls.Helper;
+
+    /// <summary>
+    /// View class shows page thumbnails from opened PDF document.
+    /// </summary>
+    public partial class PDFThumbnailView
+    {
+        #region Private methods - render related
+
+        private void RenderPages(DrawingContext drawingContext)
+        {
+            // Draw background
+            drawingContext.DrawRectangle(Background, null, new Rect(0, 0, ViewportWidth, ViewportHeight));
+
+            var pageOnCenter = _renderedPages.FirstOrDefault(page => page.IsOnCenter);
+            // Determine pages to draw.
+            _renderedPages.Clear();
+            _renderedPages.AddRange(PDFPageComponent.DeterminePagesToRender(
+                VerticalOffset,
+                VerticalOffset + ViewportHeight,
+                2d * FontSize,
+                _thumbnailZoomFactor));
+
+            // Determine viewport rectangle
+            var viewportRectangle = new Rect(0, 0, _viewport.Width, _viewport.Height);
+
+            // Iterate the pages, adjust some values, and draw them.
+            foreach (var pageInfo in _renderedPages)
+            {
+                // Current page width
+                var currentPageWidth = pageInfo.Page.Width * _thumbnailZoomFactor;
+
+                // Center the page horizontally
+                pageInfo.Left = ViewportWidth / 2d - currentPageWidth / 2d;
+                pageInfo.Right = ViewportWidth / 2d + currentPageWidth / 2d;
+
+                // Take offsets into account
+                pageInfo.Left -= HorizontalOffset;
+                pageInfo.Right -= HorizontalOffset;
+                pageInfo.Top -= VerticalOffset;
+                pageInfo.Bottom -= VerticalOffset;
+
+                var pageRect = new Rect(pageInfo.Left, pageInfo.Top, Math.Max(1d, pageInfo.Right - pageInfo.Left), Math.Max(0d, pageInfo.Bottom - pageInfo.Top));
+
+                var pageOnViewport = pageRect;
+                pageOnViewport.Intersect(viewportRectangle);
+                if (pageOnViewport.IsEmpty)
+                {
+                    continue;
+                }
+
+                pageOnViewport.Width = Math.Max(1d, pageOnViewport.Width);
+                pageOnViewport.Height = Math.Max(1d, pageOnViewport.Height);
+
+                var pageRectForPDFium = pageRect;
+                pageRectForPDFium.Y = pageRect.Y > 0d ? 0d : pageRect.Y;
+                pageRectForPDFium.X = pageRect.X > 0d ? 0d : pageRect.X;
+                pageRectForPDFium.Width = Math.Max(1d, pageRectForPDFium.Width);
+                pageRectForPDFium.Height = Math.Max(1d, pageRectForPDFium.Height);
+
+                // Draw page background
+                drawingContext.DrawRectangle(PDFPageBackground, null, new Rect(pageRect.TopLeft, pageRect.BottomRight));
+
+                try
+                {
+                    var bitmap = new WriteableBitmap((int)pageOnViewport.Width, (int)pageOnViewport.Height, 96, 96, PixelFormats.Bgra32, null);
+                    var format = BitmapFormatConverter.GetFormat(bitmap.Format);
+
+                    bitmap.Lock();
+                    pageInfo.Page.RenderPageBitmap(
+                        _thumbnailZoomFactor,
+                        (int)pageRectForPDFium.X, (int)pageRectForPDFium.Y, (int)pageRectForPDFium.Width, (int)pageRectForPDFium.Height,
+                        (int)pageOnViewport.Width, (int)pageOnViewport.Height, format, bitmap.BackBuffer, bitmap.BackBufferStride);
+                    bitmap.AddDirtyRect(new Int32Rect(0, 0, (int)pageOnViewport.Width, (int)pageOnViewport.Height));
+                    bitmap.Unlock();
+
+                    // Draw page content.
+                    drawingContext.DrawImage(bitmap, new Rect(pageOnViewport.TopLeft, pageOnViewport.BottomRight));
+                }
+                catch { }
+
+                // Draw page label
+                var ft = new FormattedText(
+                    pageInfo.Page.PageLabel,
+                    CultureInfo.InvariantCulture,
+                    FlowDirection.LeftToRight,
+                    new Typeface(FontFamily, FontStyle, FontWeight, FontStretch),
+                    FontSize,
+                    Foreground,
+                    VisualTreeHelper.GetDpi(this).PixelsPerDip);
+                var textLocation = new Point(pageInfo.Left + (pageInfo.Right - pageInfo.Left) / 2d - ft.WidthIncludingTrailingWhitespace / 2, pageInfo.Bottom);
+                drawingContext.DrawText(ft, textLocation);
+
+                // Draw page border - left
+                drawingContext.DrawLine(new Pen(PDFPageBorderBrush, PDFPageBorderThickness.Left), new Point(pageInfo.Left, pageInfo.Top), new Point(pageInfo.Left, pageInfo.Bottom));
+                // Draw page border - top
+                drawingContext.DrawLine(new Pen(PDFPageBorderBrush, PDFPageBorderThickness.Top), new Point(pageInfo.Left, pageInfo.Top), new Point(pageInfo.Right, pageInfo.Top));
+                // Draw page border - right
+                drawingContext.DrawLine(new Pen(PDFPageBorderBrush, PDFPageBorderThickness.Right), new Point(pageInfo.Right, pageInfo.Top), new Point(pageInfo.Right, pageInfo.Bottom));
+                // Draw page border - bottom
+                drawingContext.DrawLine(new Pen(PDFPageBorderBrush, PDFPageBorderThickness.Bottom), new Point(pageInfo.Left, pageInfo.Bottom), new Point(pageInfo.Right, pageInfo.Bottom));
+            }
+
+            // Draw background border - left
+            drawingContext.DrawLine(new Pen(BorderBrush, BorderThickness.Left), new Point(0, 0), new Point(0, ViewportHeight));
+            // Draw background border - top
+            drawingContext.DrawLine(new Pen(BorderBrush, BorderThickness.Top), new Point(0, 0), new Point(ViewportWidth, 0));
+            // Draw background border - right
+            drawingContext.DrawLine(new Pen(BorderBrush, BorderThickness.Right), new Point(ViewportWidth, 0), new Point(ViewportWidth, ViewportHeight));
+            // Draw background border - bottom
+            drawingContext.DrawLine(new Pen(BorderBrush, BorderThickness.Bottom), new Point(0, ViewportHeight), new Point(ViewportWidth, ViewportHeight));
+        }
+
+        private void RenderEmptyArea(DrawingContext drawingContext)
+        {
+            // Draw background
+            drawingContext.DrawRectangle(Background, null, new Rect(0, 0, ViewportWidth, ViewportHeight));
+            // Draw page border - left
+            drawingContext.DrawLine(new Pen(BorderBrush, BorderThickness.Left), new Point(0, 0), new Point(0, ViewportHeight));
+            // Draw page border - top
+            drawingContext.DrawLine(new Pen(BorderBrush, BorderThickness.Top), new Point(0, 0), new Point(ViewportWidth, 0));
+            // Draw page border - right
+            drawingContext.DrawLine(new Pen(BorderBrush, BorderThickness.Right), new Point(ViewportWidth, 0), new Point(ViewportWidth, ViewportHeight));
+            // Draw page border - bottom
+            drawingContext.DrawLine(new Pen(BorderBrush, BorderThickness.Bottom), new Point(0, ViewportHeight), new Point(ViewportWidth, ViewportHeight));
+        }
+
+        #endregion Private methods - render related
+    }
+}
