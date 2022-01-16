9daecd04904cd346d908110304bf5e5072ab84b6
Miloš Konečný <milos.konecny@outlook.com>
2022-01-16 18:36:02 +0100

    Fix warnings in WinFormsControls module
    This commit will fix some warnings in PDFiumDotNET.WinFormsControls assembly.

--- Sources/Controls/WinFormsControls/PDFThumbnailControl.cs
+++ Sources/Controls/WinFormsControls/PDFThumbnailControl.cs
@@ -1,17 +1,15 @@
 ﻿namespace PDFiumDotNET.WinFormsControls
 {
-    using System;
-    using System.Collections.Generic;
-    using System.ComponentModel;
-    using System.Data;
-    using System.Drawing;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
     using System.Windows.Forms;
 
+    /// <summary>
+    /// Class implements control to view thumbnails of PDF document.
+    /// </summary>
     public partial class PDFThumbnailControl : UserControl
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PDFThumbnailControl"/> class.
+        /// </summary>
         public PDFThumbnailControl()
         {
             InitializeComponent();
