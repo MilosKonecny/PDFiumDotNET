ea7e46bdca1dfa6aa1331f3c42184c004b953fba
Miloš Konečný <milos.konecny@outlook.com>
2023-04-04 15:01:47 +0200

    Add missing file (#113)
    This commit will add missing file.

---
+++ Sources/Controls/WpfControls/Constants.cs
@@ -0,0 +1,13 @@
+﻿namespace PDFiumDotNET.WpfControls
+{
+    /// <summary>
+    /// Static class contains project constants.
+    /// </summary>
+    internal static class Constants
+    {
+        /// <summary>
+        /// Category name of dependency properties.
+        /// </summary>
+        public const string PDFiumDotNETPropertyCategory = "PDFiumDotNET";
+    }
+}
