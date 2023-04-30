a441ff8e4917b414ffd1f600eaedc590da852756
Miloš Konečný <milos.konecny@outlook.com>
2023-04-30 20:35:41 +0200

    Extend test application (#122)
    This commit will extend the test application.

---
+++ Sources/Apps/Common/CommonInformation.cs
@@ -0,0 +1,60 @@
+﻿namespace PDFiumDotNET.Apps.Common
+{
+    using System.Runtime.InteropServices;
+
+    /// <summary>
+    /// The class provides build information.
+    /// </summary>
+    public static class CommonInformation
+    {
+        /// <summary>
+        /// Gets the build information.
+        /// </summary>
+        public static string Info
+        {
+            get
+            {
+                return Framework + " / " + Framework + " / " + Configuration;
+            }
+        }
+
+        /// <summary>
+        /// Gets the build configuration.
+        /// </summary>
+        public static string Configuration
+        {
+            get
+            {
+#if DEBUG
+                return "Debug";
+#elif RELEASE
+                return "Release";
+#else
+                return "? ? ?";
+#endif
+            }
+        }
+
+        /// <summary>
+        /// Gets the framework text.
+        /// </summary>
+        public static string Framework
+        {
+            get
+            {
+                return RuntimeInformation.FrameworkDescription;
+            }
+        }
+
+        /// <summary>
+        /// Gets the architecture text.
+        /// </summary>
+        public static string Architecture
+        {
+            get
+            {
+                return RuntimeInformation.ProcessArchitecture.ToString();
+            }
+        }
+    }
+}
--- Sources/Apps/ComponentsTest/Program.cs
+++ Sources/Apps/ComponentsTest/Program.cs
@@ -2,7 +2,6 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Diagnostics;
     using System.Reflection;
     using System.Runtime.InteropServices;
     using System.Windows;
@@ -25,51 +24,124 @@
         private const string _pdfFile = @"..\..\..\..\..\..\TestData\PDFs\Precalculus.pdf";
         private static int _testCount = 1000;
         private static int _executedOperationGroups = 0;
-        private static MemoryUsage _memoryUsage = new();
+        private static MemoryUsage _memoryUsage = new ();
+        private static bool _cancelOperation = false;
 
-        private static List<Tuple<char, ConsoleKey, string, Action<string>>> _tests = new ()
+        private static List<ActionDescription> _tests = new ()
         {
-            Tuple.Create('\0', ConsoleKey.UpArrow, "Increase 'several times' for operation group", Increase1TestCount),
-            Tuple.Create('\0', ConsoleKey.RightArrow, "Increase 'several times' by 100 for operation group", Increase100TestCount),
-            Tuple.Create('\0', ConsoleKey.DownArrow, "Decrease 'several times' for operation group", Decrease1TestCount),
-            Tuple.Create('\0', ConsoleKey.LeftArrow, "Decrease 'several times' by 100 for operation group", Decrease100TestCount),
-            Tuple.Create('x', ConsoleKey.Oem102, "Exit application", ExitApplication),
-            Tuple.Create('1', ConsoleKey.Oem102, "Collect GC", FreeMemory),
-            Tuple.Create('a', ConsoleKey.Oem102, "Operation group 1 - open & close", OperationGroup1),
-            Tuple.Create('A', ConsoleKey.Oem102, "Operation group 1 (several times)", OperationGroup1SeveralTimes),
-            Tuple.Create('b', ConsoleKey.Oem102, "Operation group 2 - 1 & navigate & zoom & position", OperationGroup2),
-            Tuple.Create('B', ConsoleKey.Oem102, "Operation group 2 (several times)", OperationGroup2SeveralTimes),
-            Tuple.Create('c', ConsoleKey.Oem102, "Operation group 3 - 1 & render", OperationGroup3),
-            Tuple.Create('C', ConsoleKey.Oem102, "Operation group 3 (several times)", OperationGroup3SeveralTimes),
+            new ActionDescription(
+                '\0',
+                ConsoleKey.UpArrow,
+                "This action increases the number of times a particular group should be executed",
+                "Increase 'several times' for operation group",
+                "Increase 'several times' for operation group",
+                Increase1TestCount),
+            new ActionDescription(
+                '\0',
+                ConsoleKey.RightArrow,
+                "This action increases the number of times a particular group should be executed by 100",
+                "Increase 'several times' for operation group by 100",
+                "Increase 'several times' for operation group by 100",
+                Increase100TestCount),
+            new ActionDescription(
+                '\0',
+                ConsoleKey.DownArrow,
+                "This action decreases the number of times a particular group should be executed",
+                "Decrease 'several times' for operation group",
+                "Decrease 'several times' for operation group",
+                Decrease1TestCount),
+            new ActionDescription(
+                '\0',
+                ConsoleKey.LeftArrow,
+                "This action decreases the number of times a particular group should be executed by 100",
+                "Decrease 'several times' for operation group by 100",
+                "Decrease 'several times' for operation group by 100",
+                Decrease100TestCount),
+            new ActionDescription(
+                'x',
+                ConsoleKey.Oem102,
+                "This action terminates the application.",
+                "Exit application",
+                "Exit application",
+                ExitApplication),
+            new ActionDescription(
+                '1',
+                ConsoleKey.Oem102,
+                "This action forces an immediate garbage collection of all generations.",
+                "Collect GC",
+                "Collect GC",
+                FreeMemory),
+            new ActionDescription(
+                'a',
+                ConsoleKey.Oem102,
+                "This action opens and closes a PDF document",
+                "Open & close",
+                "Open & close (several times)",
+                OperationGroup1),
+            new ActionDescription(
+                'A',
+                ConsoleKey.Oem102,
+                "'a' several times",
+                "Open & close",
+                "Open & close (several times)",
+                OperationGroup1SeveralTimes),
+            new ActionDescription(
+                'b',
+                ConsoleKey.Oem102,
+                "This action opens, navigates to every page, gets position, zooms and closes a PDF document",
+                "Open & navigate to every page & get position & zoom & get position & close",
+                "Open & navigate to every page & get position & zoom & get position & close (several times)",
+                OperationGroup2),
+            new ActionDescription(
+                'B',
+                ConsoleKey.Oem102,
+                "'b' several times",
+                "Open & navigate to every page & get position & zoom & get position & close",
+                "Open & navigate to every page & get position & zoom & get position & close (several times)",
+                OperationGroup2SeveralTimes),
+            new ActionDescription(
+                'c',
+                ConsoleKey.Oem102,
+                "This action opens, renders every page and closes a PDF document",
+                "Open & render every page & close",
+                "Open & render every page & close (several times)",
+                OperationGroup3),
+            new ActionDescription(
+                'C',
+                ConsoleKey.Oem102,
+                "'c' several times",
+                "Open & render every page & close",
+                "Open & render every page & close (several times)",
+                OperationGroup3SeveralTimes),
         };
 
-        private static void Increase1TestCount(string description)
+        private static void Increase1TestCount(ActionDescription description)
         {
             _testCount = Math.Min(_testCount + 1, _maxTestCount);
         }
 
-        private static void Increase100TestCount(string description)
+        private static void Increase100TestCount(ActionDescription description)
         {
             _testCount = Math.Min(_testCount + 100, _maxTestCount);
         }
 
-        private static void Decrease1TestCount(string description)
+        private static void Decrease1TestCount(ActionDescription description)
         {
             _testCount = Math.Max(_testCount - 1, _minTestCount);
         }
 
-        private static void Decrease100TestCount(string description)
+        private static void Decrease100TestCount(ActionDescription description)
         {
             _testCount = Math.Max(_testCount - 100, _minTestCount);
         }
 
-        private static void FreeMemory(string description)
+        private static void FreeMemory(ActionDescription description)
         {
             GC.Collect();
             GC.WaitForPendingFinalizers();
         }
 
-        private static void OperationGroup1(string description)
+        private static void OperationGroup1(ActionDescription description)
         {
             var pdfComponent = PDFFactory.PDFComponent;
             var pageComponent = pdfComponent.LayoutComponent.CreatePageComponent("standard", PageLayoutType.Standard);
@@ -88,20 +160,26 @@
             _executedOperationGroups++;
         }
 
-        private static void OperationGroup1SeveralTimes(string description)
+        private static void OperationGroup1SeveralTimes(ActionDescription description)
         {
             for (var index = 0; index < _testCount; index++)
             {
-                SetProgress1(description, index, _testCount);
+                PrintMemoryUsage();
+                SetProgressForGroup(description.GroupText, index, _testCount);
                 OperationGroup1(description);
                 PrintMemoryUsage();
                 PrintExecutedOperationGroups();
+
+                if (_cancelOperation)
+                {
+                    break;
+                }
             }
 
-            ClearProgress1();
+            ClearProgressForGroup();
         }
 
-        private static void OperationGroup2(string description)
+        private static void OperationGroup2(ActionDescription description)
         {
             var pdfComponent = PDFFactory.PDFComponent;
             var result = pdfComponent.OpenDocument(_pdfFile);
@@ -115,6 +193,8 @@
             var zoomComponent = pageComponent.ZoomComponent;
             for (var index = 0; index < pageComponent.PageCount; index++)
             {
+                PrintMemoryUsage();
+                SetProgressForTest(description.Text, index, pageComponent.PageCount);
                 pageComponent.NavigateToPage(index + 1);
                 var position1 = pageComponent.RenderManager.DeterminePagePosition(index);
                 if (index % 2 == 0)
@@ -127,28 +207,41 @@
                 }
 
                 var position2 = pageComponent.RenderManager.DeterminePagePosition(index);
+
+                if (_cancelOperation)
+                {
+                    break;
+                }
             }
 
             pdfComponent.CloseDocument();
             pdfComponent.Dispose();
 
+            ClearProgressForTest();
+
             _executedOperationGroups++;
         }
 
-        private static void OperationGroup2SeveralTimes(string description)
+        private static void OperationGroup2SeveralTimes(ActionDescription description)
         {
             for (var index = 0; index < _testCount; index++)
             {
-                SetProgress1(description, index, _testCount);
+                PrintMemoryUsage();
+                SetProgressForGroup(description.GroupText, index, _testCount);
                 OperationGroup2(description);
                 PrintMemoryUsage();
                 PrintExecutedOperationGroups();
+
+                if (_cancelOperation)
+                {
+                    break;
+                }
             }
 
-            ClearProgress1();
+            ClearProgressForGroup();
         }
 
-        private static void OperationGroup3(string description)
+        private static void OperationGroup3(ActionDescription description)
         {
             var pdfComponent = PDFFactory.PDFComponent;
             var pageComponent = pdfComponent.LayoutComponent.CreatePageComponent("standard", PageLayoutType.Standard);
@@ -164,7 +257,7 @@
             for (var index = 0; index < pageComponent.PageCount; index++)
             {
                 PrintMemoryUsage();
-                SetProgress2(description, index, pageComponent.PageCount);
+                SetProgressForTest(description.Text, index, pageComponent.PageCount);
                 var page = pageComponent.Pages[index];
 
                 var bitmap = new WriteableBitmap((int)page.Width, (int)page.Height, 96, 96, PixelFormats.Bgra32, null);
@@ -184,30 +277,41 @@
                     bitmap.BackBufferStride);
                 bitmap.AddDirtyRect(new Int32Rect(0, 0, (int)page.Width, (int)page.Height));
                 bitmap.Unlock();
+
+                if (_cancelOperation)
+                {
+                    break;
+                }
             }
 
             pdfComponent.CloseDocument();
             pdfComponent.Dispose();
 
-            ClearProgress2();
+            ClearProgressForTest();
 
             _executedOperationGroups++;
         }
 
-        private static void OperationGroup3SeveralTimes(string description)
+        private static void OperationGroup3SeveralTimes(ActionDescription description)
         {
             for (var index = 0; index < _testCount; index++)
             {
-                SetProgress1(description, index, _testCount);
+                PrintMemoryUsage();
+                SetProgressForGroup(description.GroupText, index, _testCount);
                 OperationGroup3(description);
                 PrintMemoryUsage();
                 PrintExecutedOperationGroups();
+
+                if (_cancelOperation)
+                {
+                    break;
+                }
             }
 
-            ClearProgress1();
+            ClearProgressForGroup();
         }
 
-        private static void ExitApplication(string description)
+        private static void ExitApplication(ActionDescription description)
         {
             throw new TerminateApplicationRequiredException();
         }
@@ -215,8 +319,7 @@
         private static void PrintStartInfo()
         {
             Console.SetCursorPosition(0, 0);
-            Console.WriteLine(RuntimeInformation.FrameworkDescription + " / " + RuntimeInformation.ProcessArchitecture);
-
+            Console.WriteLine(CommonInformation.Info);
             Console.ForegroundColor = ConsoleColor.Green;
             Console.SetCursorPosition(0, 1);
             Console.WriteLine($"Memory usage after start: {_memoryUsage.CurrentMemoryUsage} KiB       ");
@@ -228,7 +331,7 @@
             _memoryUsage.GatherMemoryUsage();
             Console.ForegroundColor = ConsoleColor.Green;
             Console.SetCursorPosition(0, 2);
-            Console.WriteLine($"Memory usage: {_memoryUsage.CurrentMemoryUsage} KiB (min: {_memoryUsage.MinimumMemoryUsage}, max: {_memoryUsage.MaximumMemoryUsage}         ");
+            Console.WriteLine($"Memory usage: {_memoryUsage.CurrentMemoryUsage} KiB (min: {_memoryUsage.MinimumMemoryUsage}, max: {_memoryUsage.MaximumMemoryUsage})         ");
             Console.ResetColor();
         }
 
@@ -251,8 +354,8 @@
             Console.WriteLine($"Available commands (count: {_tests.Count})    ");
             foreach (var test in _tests)
             {
-                object key = test.Item1 != '\0' ? test.Item1 : test.Item2;
-                Console.WriteLine($"\t{key} - {test.Item3}");
+                object key = test.ActionCharacter != '\0' ? test.ActionCharacter : test.ActionConsoleKey;
+                Console.WriteLine($"  {key} - {test.Description}");
             }
         }
 
@@ -271,26 +374,26 @@
             Console.WriteLine(text);
         }
 
-        private static void SetProgress1(string text, int position, int length)
+        private static void SetProgressForGroup(string text, int position, int length)
         {
             Console.SetCursorPosition(0, 10 + _tests.Count);
             Console.WriteLine($"{text}: {100 * position / length}%");
         }
 
-        private static void ClearProgress1()
+        private static void ClearProgressForGroup()
         {
             var text = new string(' ', Console.BufferWidth);
             Console.SetCursorPosition(0, 10 + _tests.Count);
             Console.WriteLine(text);
         }
 
-        private static void SetProgress2(string text, int position, int length)
+        private static void SetProgressForTest(string text, int position, int length)
         {
             Console.SetCursorPosition(0, 11 + _tests.Count);
             Console.WriteLine($"{text}: {100 * position / length}%");
         }
 
-        private static void ClearProgress2()
+        private static void ClearProgressForTest()
         {
             var text = new string(' ', Console.BufferWidth);
             Console.SetCursorPosition(0, 11 + _tests.Count);
@@ -302,10 +405,11 @@
             ClearError();
             foreach (var test in _tests)
             {
-                if ((test.Item1 != '\0' && test.Item1 == key.KeyChar)
-                    || (test.Item2 != ConsoleKey.Oem102 && test.Item2 == key.Key))
+                if ((test.ActionCharacter != '\0' && test.ActionCharacter == key.KeyChar)
+                    || (test.ActionConsoleKey != ConsoleKey.Oem102 && test.ActionConsoleKey == key.Key))
                 {
-                    test.Item4(test.Item3);
+                    test.Action(test);
+                    PrintMemoryUsage();
                     break;
                 }
             }
@@ -316,25 +420,39 @@
         /// </summary>
         public static void Main()
         {
+            Console.CancelKeyPress += (s, e) =>
+            {
+                _cancelOperation = true;
+                e.Cancel = true;
+            };
+
             try
             {
-                Console.Title = Assembly.GetEntryAssembly().GetName().Name;
-                Console.CursorVisible = false;
-                Console.Clear();
-
-                PrintStartInfo();
                 while (true)
                 {
-                    PrintMemoryUsage();
-                    PrintExecutedOperationGroups();
-                    PrintAvailableCommands();
+                    try
+                    {
+                        Console.Title = Assembly.GetEntryAssembly().GetName().Name;
+                        Console.CursorVisible = false;
+                        Console.Clear();
 
-                    var key = Console.ReadKey(true);
-                    ProcessCommand(key);
+                        PrintStartInfo();
+                        while (true)
+                        {
+                            PrintMemoryUsage();
+                            PrintExecutedOperationGroups();
+                            PrintAvailableCommands();
+
+                            var key = Console.ReadKey(true);
+                            ProcessCommand(key);
+                            _cancelOperation = false;
+                        }
+                    }
+                    catch (TerminateApplicationRequiredException)
+                    {
+                        break;
+                    }
                 }
-            }
-            catch (TerminateApplicationRequiredException)
-            {
             }
             catch (Exception ex)
             {
