c31a2293879699d5aaeaab88d607a2ce2900df83
Miloš Konečný <milos.konecny@outlook.com>
2023-05-09 12:25:24 +0200

    Add initial version of PDF merge (#124)
    This commit will add initial version of PDF merge application.

---
+++ Sources/Apps/PDFMerge/Contracts/IViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace PDFiumDotNET.Apps.PDFMerge.Contracts
+{
+    /// <summary>
+    /// Interface defines functionality of view model visible to view.
+    /// </summary>
+    public interface IViewModel
+    {
+        /// <summary>
+        /// Called after view model was assigned to the view.
+        /// </summary>
+        /// <param name="view">View where is view model assigned.</param>
+        void AssignedToView(IView view);
+    }
+}
