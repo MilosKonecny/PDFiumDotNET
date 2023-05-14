64aca2ff76cefd65eb2280b1fccc2672ac8085ff
Miloš Konečný <milos.konecny@outlook.com>
2023-05-14 18:24:47 +0200

    Add script to create NuGet packages (#132)
    This commit will add changes to create NuGet packages.

---
+++ Sources/CreatePackages.ps1
@@ -0,0 +1,25 @@
+#dotnet clean --configuration Release
+
+dotnet build --configuration Release
+
+# Create and empty release folder
+$release_folder=".\..\Release\"
+$nuget_folder=".\..\Release\NuGet\"
+if (!(Test-Path $release_folder))
+{
+	New-Item $release_folder -ItemType Directory
+}
+if (!(Test-Path $nuget_folder))
+{
+	New-Item $nuget_folder -ItemType Directory
+}
+
+Get-ChildItem -Path $release_folder -Include *.* -File -Recurse | foreach { $_.Delete()}
+
+Copy-Item ".\Wrapper\Wrapper\bin\Release\*.nupkg" $nuget_folder
+Copy-Item ".\Components\Components.Contracts\bin\Release\*.nupkg" $nuget_folder
+Copy-Item ".\Components\Components\bin\Release\*.nupkg" $nuget_folder
+Copy-Item ".\Components\Components.Factory\bin\Release\*.nupkg" $nuget_folder
+Copy-Item ".\Controls\WpfControls\bin\Release\*.nupkg" $nuget_folder
+
+pause
