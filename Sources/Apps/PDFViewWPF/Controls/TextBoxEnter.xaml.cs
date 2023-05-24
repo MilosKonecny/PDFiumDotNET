f9acd4476b9561488d477137f6a36ae3cc6d3bd4
Miloš Konečný <milos.konecny@outlook.com>
2023-05-24 21:48:29 +0200

    Eliminate ms behaviors assembly (#146)
    This commit will eliminate Microsoft.Xaml.Behaviors.

---
+++ Sources/Apps/PDFViewWPF/Controls/TextBoxEnter.xaml.cs
@@ -0,0 +1,40 @@
+﻿namespace PDFiumDotNET.Apps.PDFViewWPF.Controls
+{
+    using System.Threading;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Input;
+    using System.Windows.Threading;
+
+    /// <summary>
+    /// Interaction logic for TextBoxEnter control.
+    /// </summary>
+    public partial class TextBoxEnter : TextBox
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TextBoxEnter"/> class.
+        /// </summary>
+        public TextBoxEnter()
+        {
+            InitializeComponent();
+        }
+
+        /// <inheritdoc/>
+        protected override void OnGotFocus(RoutedEventArgs e)
+        {
+            base.OnGotFocus(e);
+            Dispatcher.BeginInvoke(() => SelectAll, DispatcherPriority.SystemIdle);
+        }
+
+        /// <inheritdoc/>
+        protected override void OnPreviewKeyDown(KeyEventArgs e)
+        {
+            base.OnPreviewKeyDown(e);
+            if (e != null && e.Key == Key.Enter)
+            {
+                BindingOperations.GetBindingExpression(this, TextBox.TextProperty).UpdateSource();
+            }
+        }
+    }
+}
