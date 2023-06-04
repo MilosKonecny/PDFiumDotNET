2de7311e109778dd0717f71177610bede86d3eb9
Miloš Konečný <milos.konecny@outlook.com>
2023-06-04 10:49:31 +0200

    Change version (#150)
    This commit will change the version of all assemblies and NuGet packages.

--- Sources/Common/AssemblyGlobalInfo.cs
+++ Sources/Common/AssemblyGlobalInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("Miloš Konečný")]
 [assembly: AssemblyProduct("PDFiumDotNET")]
-[assembly: AssemblyCopyright("Copyright © Miloš Konečný 2020")]
+[assembly: AssemblyCopyright("Copyright © Miloš Konečný 2020-2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
 
@@ -13,8 +13,8 @@ using System.Runtime.InteropServices;
 
 [assembly: NeutralResourcesLanguage("en", UltimateResourceFallbackLocation.MainAssembly)]
 
-[assembly: AssemblyVersion("1.00.00.000")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.00.01.000")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
