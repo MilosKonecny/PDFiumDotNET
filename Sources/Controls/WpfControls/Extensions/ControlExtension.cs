5d5a95e8ac20bcbdc0aca65e461b1dd3613c0272
Miloš Konečný <milos.konecny@outlook.com>
2023-11-01 09:55:04 +0100

    Add properties to WPF PDFView (#131)
    This commit will add some properties to control GC and redraw optimization.

---
+++ Sources/Controls/WpfControls/Extensions/ControlExtension.cs
@@ -0,0 +1,22 @@
+﻿namespace PDFiumDotNET.WpfControls.Extensions
+{
+    using System.ComponentModel;
+    using System.Windows;
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Extension class for <see cref="Control"/>.
+    /// </summary>
+    internal static class ControlExtension
+    {
+        /// <summary>
+        /// Gets the information whether the model is used in design time.
+        /// </summary>
+        /// <param name="control">Instance of calling <see cref="Control"/>.</param>
+        /// <returns><c>true</c> if the design time is active; otherwise <c>false</c>.</returns>
+        public static bool IsDesignTime(this Control control)
+        {
+            return DesignerProperties.GetIsInDesignMode(new DependencyObject());
+        }
+    }
+}
